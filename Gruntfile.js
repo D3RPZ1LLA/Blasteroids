cbbb4cbe6982c5d24fc5b7d7e8f23d139f877fc9
D3RPZ1LLA <d3rpz1lla@gmail.com>
2016-07-11 14:18:05 -0700

    init

---
+++ Gruntfile.js
@@ -0,0 +1,87 @@
+'use strict';
+
+var path = require('path')
+
+module.exports = function (grunt) {
+
+  [
+    'grunt-contrib-coffee',
+    'grunt-contrib-jade',
+    'grunt-contrib-stylus',
+    'grunt-exec',
+    'grunt-regarde'
+  ].forEach(grunt.loadNpmTasks);
+
+  grunt.initConfig({
+    exec: {
+      app: {
+        cmd: 'node app'
+      }
+    },
+
+    watch: {
+      stylus: {
+        files: ['**/*.styl'],
+        tasks: ['stylus']
+      },
+      jade: {
+        files: ['**/*.jade'],
+        tasks: ['jade']
+      },
+      coffee: {
+        files: ['**/*.coffee'],
+        tasks: ['coffee']
+      }
+    },
+
+    coffee: {
+      dist: {
+        expand: true,
+        cwd: 'src',
+        src: ['**/*.coffee'],
+        dest: 'dist',
+        ext: '.js'
+      }
+    },
+
+    jade: {
+      compile: {
+        options: {
+          pretty: true
+        },
+        files: [
+          {
+            expand: true,
+            cwd: 'src',
+            src: '**/**.jade',
+            dest: 'dist',
+            ext: '.html'
+          }
+        ]
+      }
+    },
+
+    stylus: {
+      compile: {
+        options: {
+          'include css': true,
+          'import': ['nib']
+        },
+        files: {
+          'dist/style.css': ['src/styl/main.styl']
+        }
+      }
+    }
+  });
+
+  grunt.renameTask('regarde', 'watch');
+  grunt.registerTask('build', [
+    'jade',
+    'stylus',
+    'coffee'
+  ]);
+  grunt.registerTask('default', [
+    'build',
+    'watch'
+  ]);
+};
---
+++ app.js
@@ -0,0 +1,19 @@
+var port = process.env.PORT || 3000
+  , app = require('express')()
+  , favicon = require('serve-favicon')
+  , device = require('express-device')
+  , server = require('http').Server(app);
+
+app.use( device.capture() );
+
+server.listen(port);
+console.log('\nϟϟϟ Serving on port ' + port + ' ϟϟϟ\n');
+
+app.get('/', function (req, res) {
+  res.sendfile('dist/' + '/index.html');
+});
+
+app.get('/*' , function( req, res, next ) {
+    var file = req.params[0];
+    res.sendfile( __dirname + '/' + file );
+});
